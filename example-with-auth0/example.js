f694fc59ca233eadf443f2c4c64777f30639d230
Shuhei Hiya <syuhei176@gmail.com>
2015-04-22 21:24:54 +0900

    Updated

--- example-with-auth0/example.js
+++ example-with-auth0/example.js
@@ -2,12 +2,12 @@ var MilkCocoa = require("milkcocoa");
 var auth0 = require("./auth0wrapper");
 
 
-auth0.setHost("milkcocoa.auth0.com");
-auth0.setClientId("z2RcQboX6E8c2yxUPZIqPaA6rdQdXQUF");
-auth0.setConnection("devices");
+auth0.setHost("{auth0 domain}");
+auth0.setClientId("{auth0 client id}");
+auth0.setConnection("{connection name}");
 
 
-var milkcocoa = new MilkCocoa("unii8fmjtlc.mlkcca.com", { file_path : "./" });
+var milkcocoa = new MilkCocoa("{milkcocoa app id}.mlkcca.com", { file_path : "./" });
 
 
 milkcocoa.user(function(err, user) {
@@ -15,7 +15,7 @@ milkcocoa.user(function(err, user) {
 	if(user) {
 		main(milkcocoa);
 	}else{
-		auth0.loginWithEmailAndPassword("syuhei176@gmail.com", "universe", function(err, data) {
+		auth0.loginWithEmailAndPassword("{email address}", "{password}", function(err, data) {
 			milkcocoa.authWithToken(data.id_token, function(err, user){
 				main(milkcocoa);
 			});
