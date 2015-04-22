7c52682fe7ec8944b5b64c71e5a5004526809655
Shuhei Hiya <syuhei176@gmail.com>
2015-04-22 21:22:51 +0900

    Updated

--- example-with-auth0/example.js
+++ example-with-auth0/example.js
@@ -6,31 +6,38 @@ auth0.setHost("milkcocoa.auth0.com");
 auth0.setClientId("z2RcQboX6E8c2yxUPZIqPaA6rdQdXQUF");
 auth0.setConnection("devices");
 
-var milkcocoa = new MilkCocoa("unii8fmjtlc.mlkcca.com", {
-	file_path : "./"
-});
+
+var milkcocoa = new MilkCocoa("unii8fmjtlc.mlkcca.com", { file_path : "./" });
+
 
 milkcocoa.user(function(err, user) {
 	console.log(err, user);
+	if(user) {
+		main(milkcocoa);
+	}else{
+		auth0.loginWithEmailAndPassword("syuhei176@gmail.com", "universe", function(err, data) {
+			milkcocoa.authWithToken(data.id_token, function(err, user){
+				main(milkcocoa);
+			});
+		});
+	}
 });
 
-auth0.loginWithEmailAndPassword("syuhei176@gmail.com", "universe", function(err, data) {
-	milkcocoa.authWithToken(data.id_token, function(err, user){
-		console.log(user);
-		main(milkcocoa);
-	});
-});
 
 
 function main(milkcocoa) {
 	var ds = milkcocoa.dataStore("logs");
 
+	ds.get("e2s6dvrrftl0mpulq", function(e) {
+		console.log(e);
+	});
+
 	ds.on("push", function(r) {
-		console.log(r);
+		console.log("a", r);
 	});
 
 	setInterval(function() {
-		ds.push({date : new Date().getTime(), content : "こんにちは"});
+		ds.push({date : new Date().getTime(), content : "a"});
 	}, 1000);	
 }
 
